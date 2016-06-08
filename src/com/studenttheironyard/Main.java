63096806949995878e27bbb3743b697a0b42018f
hoseasandstrom <Lakyn4ever>
2016-06-08 10:47:28 -0400

    Initial setup

---
+++ src/com/studenttheironyard/Main.java
@@ -0,0 +1,26 @@
+package com.studenttheironyard;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    static HashMap<String, User> users = new HashMap<>();
+    static ArrayList<Message> messages = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+
+    static void addTestUsers() {
+        users.put("Alice", new User("Alice", ""));
+        users.put("Bob", new User("Bob", ""));
+        users.put("Charlie", new User("Charlie", ""));
+    }
+
+    static void addTestMessages() {
+        messages.add(new Message(0, -1, "Alice", "Hello world!"));
+        messages.add(new Message(1, -1, "Bob", "This is a new thread!"));
+        messages.add(new Message(2, 0, "Charlie", ));
+    }
+}
---
+++ src/com/studenttheironyard/Message.java
@@ -0,0 +1,9 @@
+package com.studenttheironyard;
+
+/**
+ * Created by hoseasandstrom on 6/8/16.
+ */
+public class Message {
+    int id;
+    int replyId;
+}
